b87fc0a218cdfb030f5ccc3fbfdd58dd193bc344
Marcelo Muñoz <leonardo.munoz@dinardap.gob.ec>
2021-01-08 15:22:54 -0500

    jsf gris detalle campos, dtd

---
+++ src/main/java/ec/gob/dinardap/consulta/controller/DetalleCamposCtrl.java
@@ -0,0 +1,15 @@
+package ec.gob.dinardap.consulta.controller;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named("detalleCamposCtrl")
+@ViewScoped
+public class DetalleCamposCtrl {
+
+	
+	public DetalleCamposCtrl() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/ec/gob/dinardap/consulta/dtd/CamposDtd.java
@@ -0,0 +1,19 @@
+package ec.gob.dinardap.consulta.dtd;
+
+public class CamposDtd {
+	private String campo;
+	private String valor;
+	
+	public String getCampo() {
+		return campo;
+	}
+	public void setCampo(String campo) {
+		this.campo = campo;
+	}
+	public String getValor() {
+		return valor;
+	}
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+}
